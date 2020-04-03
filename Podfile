68492fc1e1f6a0b06e2a133a7556750cbe06dca0
Nick Shields <Nick_Shields@ultimatesoftware.com>
2020-04-02 23:07:22 -0400

    This is a working rtmp stream publishing iPhone App

---
+++ Podfile
@@ -0,0 +1,10 @@
+use_frameworks!
+
+def import_pods
+    pod 'HaishinKit', '~> 1.0.7'
+end
+
+target 'Streamable'  do
+    platform :ios, '8.0'
+    import_pods
+end
--- Streamable/ViewController.swift
+++ Streamable/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import HaishinKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupAudio()
+        self.rtmp_publish_stream()
         // Do any additional setup after loading the view.
+    }
+    
+    func setupAudio(){
+        print("In here")
+        let session = AVAudioSession.sharedInstance()
+        do{
+            if #available(iOS 10.0, *){
+                print("This is what I did")
+                try session.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth])
+            }else{
+                session.perform(NSSelectorFromString("setCategory:withOptions:error:"), with: AVAudioSession.Category.playAndRecord,
+                                with: [AVAudioSession.CategoryOptions.allowBluetooth, AVAudioSession.CategoryOptions.defaultToSpeaker])
+                try session.setMode(.default)
+            }
+            try session.setActive(true)
+        }catch{
+            print(error)
+        }
+    }
+    
+    func rtmp_publish_stream(){
+        let rtmpConnection = RTMPConnection()
+        let rtmpStream = RTMPStream(connection: rtmpConnection)
+        rtmpStream.attachAudio(AVCaptureDevice.default(for: AVMediaType.audio)) { error in
+            print(error)
+        }
+        rtmpStream.attachCamera(DeviceUtil.device(withPosition: .back)) { error in
+            print("SHIT")
+            print(error)
+        }
+        
+        let hkView = HKView(frame: view.bounds)
+        hkView.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        hkView.attachStream(rtmpStream)
+        
+        view.addSubview(hkView)
+        
+        rtmpConnection.connect("rtmp://192.168.1.11:32782/show")
+        rtmpStream.publish("test")
+    
     }
 
 
